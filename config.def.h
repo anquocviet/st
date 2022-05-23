488851a6935baff01518d55361b1ccee51e4d62e
anquocviet <anq.viet203@gmail.com>
2022-05-23 14:05:27 +0700

    Change default font to JetBrains Mono

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:pixelsize=16:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:style=Regular:pixelsize=16:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"Noto Color Emoji:pixelsize=15:antialias=true:autohint=true",
