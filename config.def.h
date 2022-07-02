db3255d2e4971fbf379d7bacc1aa54f7332e412c
anquocviet <an.q.viet203@gmail.com>
2022-07-02 22:30:24 +0700

    Change my default font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:style=Regular:pixelsize=16:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font Mono:style=Regular:pixelsize=16:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"Noto Color Emoji:style=Regular:pixelsize=15:antialias=true:autohint=false",
